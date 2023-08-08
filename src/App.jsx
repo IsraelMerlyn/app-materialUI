9f6855f2e786a50b0d39f55de74f49ba5ac72574
IsraelMerlyn <merlyn142a@gmail.com>
2023-08-08 17:31:37 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import { Button } from '@mui/material'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+    <h1>Material UI</h1>
+    <Button variant="contained">Hola</Button>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ButtonC.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material"
+
+
+export const ButtonC = () => {
+  return (
+   <>
+    <Button variant="text">Text</Button>
+    <Button variant="contained">Contained</Button>
+    <Button variant="outlined">Outlined</Button>
+   </>
+  )
+}
